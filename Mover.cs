afdec7285c1789d74878063926113bfa2ae2a17f
Fanderz <135126601+Fanderz@users.noreply.github.com>
2024-08-23 17:40:53 +0300

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform _point;
+    [SerializeField] private float _speed;
+
+    private Transform[] _childPoints;
+    private int _pointIndex = 0;
+
+    private void Start()
+    {
+        _childPoints = new Transform[_point.childCount];
+
+        for (int i = 0; i < _point.childCount; i++)
+            _childPoints[i] = _point.GetChild(i).GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Transform target = _childPoints[_pointIndex];
+
+        if (transform.position == target.position)
+            target.position = GetNexPoint();
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, _speed * Time.deltaTime);
+    }
+
+    private Vector3 GetNexPoint()
+    {
+        if (_pointIndex == _childPoints.Length)
+            _pointIndex = 0;
+
+        return _childPoints[_pointIndex++].transform.position;
+    }
+}
